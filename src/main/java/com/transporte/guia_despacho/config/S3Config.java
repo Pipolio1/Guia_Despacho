a04265d8ec26e7441d89eb5d653655a06d480f73
Pipolio1 <fgennariberroeta@gmail.com>
2026-06-08 13:11:44 -0400

    Add AWS session token support for AWS Academy credentials

--- src/main/java/com/transporte/guia_despacho/config/S3Config.java
+++ src/main/java/com/transporte/guia_despacho/config/S3Config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -17,15 +18,25 @@ public class S3Config {
     @Value("${aws.secretKey}")
     private String secretKey;
 
+    @Value("${aws.sessionToken:}")
+    private String sessionToken;
+
     @Value("${aws.region:us-east-1}")
     private String region;
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretKey);
+        StaticCredentialsProvider credentialsProvider;
+        if (sessionToken != null && !sessionToken.isEmpty()) {
+            AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(accessKeyId, secretKey, sessionToken);
+            credentialsProvider = StaticCredentialsProvider.create(sessionCredentials);
+        } else {
+            AwsBasicCredentials basicCredentials = AwsBasicCredentials.create(accessKeyId, secretKey);
+            credentialsProvider = StaticCredentialsProvider.create(basicCredentials);
+        }
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
